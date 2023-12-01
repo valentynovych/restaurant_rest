292f6a5ee341fbffe4e839a01c5242f28144629b
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-12-01 21:51:42 +0200

    Initial commit

---
+++ src/main/java/com/restaurant_rest/RestaurantRestApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantRestApplication.class, args);
+    }
+
+}
