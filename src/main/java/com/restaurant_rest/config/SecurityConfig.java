bc9333ff2b42c793118b83e8a7d54e7691d34283
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-01-02 01:38:40 +0200

    disable exception handle in SecurityConfig

--- src/main/java/com/restaurant_rest/config/SecurityConfig.java
+++ src/main/java/com/restaurant_rest/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.restaurant_rest.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -46,9 +44,9 @@ public class SecurityConfig {
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(manager ->
-                        manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(exHandling ->
-                        exHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+                        manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//                .exceptionHandling(exHandling ->
+//                        exHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
